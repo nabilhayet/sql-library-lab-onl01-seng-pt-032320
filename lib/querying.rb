b8cafffa47b0c307cd28369aaa8fffadc1f962e3
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-09 04:23:44 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,5 +39,6 @@ def select_character_names_and_number_of_books_they_are_in
   from characters
   INNER JOIN character_books
   ON characters.id = character_books.character_id
-  GROUP BY Value DESC"
+  GROUP BY characters.name 
+  ORDER BY Value DESC"
 end

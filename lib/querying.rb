e48903bc7ada8e77947e1c622b817ad50615feb4
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-09 04:43:54 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,8 @@ def select_series_title_with_most_human_characters
   from series
   INNER JOIN authors
   ON series.author_id = authors.id
-  
+  INNER JOIN characters
+  ON authors.id = characters.author_id"
 end
 
 def select_character_names_and_number_of_books_they_are_in

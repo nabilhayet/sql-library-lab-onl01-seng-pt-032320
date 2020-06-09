3f500d9eb1ba5dd6336843574e4cd9aa9c8bf202
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-09 04:10:50 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "select characters.name, sum(character_books.book_id)
+  "select characters.name, count(character_books.book_id)
   from characters
   INNER JOIN character_books
   ON characters.id = character_books.character_id
